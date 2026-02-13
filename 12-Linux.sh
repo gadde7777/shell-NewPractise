17f8b333252f5a203b0b88086506fce25e0d18c3
an ga <rao2k42@gmail.com>
2026-02-13 13:25:29 -0500

    textt

---
+++ 12-Linux.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+echo " Please run with root user"
+
+fi
+
+echo "Installing Nginx"
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+echo " Installing Nginx : Failure "
+exit 1
+else
+echo "Installing Nginx IS success"
+fi
+
+echo "Installing Mysql
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+echo " Installing mysql is Failure"
+exit 1
+else
+echo "Installing mysql is success"
+fi
+
+echo "Installing nodejs
+dnf install nodejs -y
+
+if [ $? -ne 0 ]; then
+echo " Installing nodejs is Failure"
+exit 1
+else
+echo "Installing node js is siccess"
+fi
