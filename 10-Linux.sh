9cb9a0bd4fff5f1a33962567950b7ba4ed995fb8
an ga <rao2k42@gmail.com>
2026-02-13 12:42:32 -0500

    textt

--- 10-Linux.sh
+++ 10-Linux.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 NUMBER=25
 
-if [ $NUMBER -gt 20 ]; then
+if [ $NUMBER -gt 30]; then
 
 echo "Number is : $NUMBER has 25"
+
+elif [ $NUMBER -eq 25 ]; then
+
+echo " Number os : $NUMBER is equal"
+
+else
+
+echo "Number os : $NUMBER IS LESS THAN 30"
 
 fi
