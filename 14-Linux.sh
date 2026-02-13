c5c17783c3752e8c0d982b32c55cb871b70dc5f7
an ga <rao2k42@gmail.com>
2026-02-13 16:14:11 -0500

    textt

---
+++ 14-Linux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+USERID=$(id-u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [$USERID -ne 0] ; then
+echo "Please run the script with root access"
+exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+Validate()
+{
+
+if [ $1 -ne 0 ];then
+
+echo "$2 : Install failure"
+exit 1
+else
+echo "$2 : Install Success"
+fi
+}
+
+dnf install nginx -y &>> $LOGS_FILE
+Validate $? "Nginx Installation"
+
+
+dnf install mysql -y &>> $LOGS_FILE
+Validate $? "Mysql Installation"
+
+
+dnf install nodejs -y &>> $LOGS_FILE
+Validate $? "Nodejs Install"
