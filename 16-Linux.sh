e967b361ef274a815f11fadff84c3f0d6dd01241
an ga <rao2k42@gmail.com>
2026-02-13 18:48:18 -0500

    textt

---
+++ 16-Linux.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+echo "Please run with root user access"
+exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATION(){
+  if [ $1 -ne 0]; then
+  echo " $2.... Failure"| tee -a $LOGS_FILE
+  exit 1
+  else
+  echo "$2 .. Success" | tee -a $LOGS_FILE
+
+}
+
+for package in $@
+do
+
+dnf install $package -y
+Validate $? "$package Installation"
+
+done
+
+# dnf install nginx -y &>> $LOGS_FILE
+# Validate $? "Nginx Installation"
+
+
+# dnf install mysql -y &>> $LOGS_FILE
+# Validate $? "Mysql Installation"
+
+
+# dnf install nodejs -y &>> $LOGS_FILE
+# Validate $? "Nodejs Install"
