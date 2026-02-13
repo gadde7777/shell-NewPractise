4af9015b6e72717d13ea1d51f1e69f555bf5e773
an ga <rao2k42@gmail.com>
2026-02-13 12:02:59 -0500

    textt

--- 09-Linux.sh
+++ 09-Linux.sh
@@ -5,6 +5,6 @@ echo "fruits are :${Fruits[@]}"
 
 echo "fruit 1 is :${Fruits[0]}"
 
-echo "fruit 2is :${Fruits[1]}"
+echo "fruit 2 :${Fruits[1]}"
 
-echo "fruit 3s :${Fruits[2]}"
+echo "fruit 3  :${Fruits[2]}"
