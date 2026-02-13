e451831f9fdfdf5d64e4cf5457a77970fcdce86e
an ga <rao2k42@gmail.com>
2026-02-13 09:07:46 -0500

    textt

---
+++ 02-Linux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This is comment
+
+today=wednesday
+if ( today != sunday)
+{
+print ("go to school")
+}
+else
+{
+print ("Take Holiday")
+}
+
+PERSON1=SURESH
+PERSON2=RAMESH
+
+echo "$PERSON1:: HELLO $PERSON2, how are you"
+echo "$PERSON2:: HELLO $PERSON1 I am good"
