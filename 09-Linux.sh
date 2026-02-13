2adab8bc2638c64dc09359697349bf47d67e16e1
an ga <rao2k42@gmail.com>
2026-02-13 12:06:54 -0500

    textt

--- 09-Linux.sh
+++ 09-Linux.sh
@@ -3,8 +3,8 @@ Fruits=("Apple","oranges","tiger")
 
 echo "fruits are :${Fruits[@]}"
 
-echo "fruit 1 is :${Fruits[0]}"
+echo "fruit 1 is : ${Fruits[0]}"
 
-echo "fruit 2 :${Fruits[1]}"
+echo "fruit 2 :  ${Fruits[1]}"
 
 echo "fruit 3  :${Fruits[2]}"
