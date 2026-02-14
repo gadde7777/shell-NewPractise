652b57170afe68c744718e303776c086e258e18d
an ga <rao2k42@gmail.com>
2026-02-13 19:26:27 -0500

    textt

---
+++ 18-Linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# '\e[31m'
+# '\e[32m'
+# '\e[33m'
+# '\e[34m'
+# '\e[0m'
+
+echo -e "\e[31m Hello world"
+echo -e "\e[32m Hello world"
+echo -e "\e[33m Hello world"
+echo -e "\e[34m Hello World"
+echo -e "\e[0m Hello World"
+
